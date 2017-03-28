3ee3c2e187a8679cd13ce17e549c40e2945c4ee8
Etudiant <Etudiant>
2017-03-28 12:34:25 +0200

    Fonctions JavaScript

---
+++ 28 mars/Exo 7/JS/script.js
@@ -0,0 +1,22 @@
+// Créer une fonction sans paramètres
+function helloWorld(){
+
+    // Ecrire le code à éxécuter à l'appel de la fonction
+    console.log('Bonjour le monde');
+
+};
+
+// Appeler la fonction
+helloWorld();
+
+// Créer une fonction avec paramètres
+function fullName( firstName, lastName ){
+
+    console.log ('Salut ' + firstName + ' ' + lastName);
+};
+
+// Appeler la fonction en précisant les paramètres
+fullName( 'Nour', 'Rakotoson' );
+fullName( 'Julien', 'Noyer');
+fullName( 'Robert', 'Marley');
+
